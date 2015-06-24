6a0f615baadcee939c2a8e77d6d8b8ea4c87b55b
makotot <makoto.ttn@gmail.com>
2015-06-25 00:53:34 +0900

    + src

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,14 @@
 module.exports = function (grunt) {
 
-  require('jit-grunt')(grunt);
+  require('jit-grunt')(grunt, {
+    browserSync: 'grunt-browser-sync'
+  });
   require('time-grunt')(grunt);
 
   grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
     path: {
       src: './src',
       env: './dev'
@@ -11,8 +16,58 @@ module.exports = function (grunt) {
 
     clean: {
       all: ['<%= path.env %>']
+    },
+
+    assemble: {
+      options: {
+        layoutdir: '<%= path.src %>/layouts',
+        partials: ['<%= path.src %>/partials/*.hbs'],
+        helpers: [
+        ]
+      },
+      all: {
+        options: {
+          layout: 'default.hbs'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= path.src %>/pages',
+            src: '**/*.hbs',
+            dest: '<%= path.env %>'
+          }
+        ]
+      }
+    },
+
+    watch: {
+      options: {
+        spawn: false
+      },
+      html: {
+        files: ['<%= path.src %>/**/*.hbs'],
+        tasks: ['assemble']
+      }
+    },
+
+    browserSync: {
+      all: {
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: '<%= path.env %>'
+          }
+        },
+        bsFiles: {
+          src: [
+            '<%= path.env %>/**/*.html'
+          ]
+        }
+      }
     }
   });
 
+
   grunt.registerTask('default', ['clean']);
+  grunt.registerTask('serve', ['clean', 'assemble', 'browserSync', 'watch']);
 };
